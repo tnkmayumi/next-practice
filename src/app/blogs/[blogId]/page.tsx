b1839ddde383abc4239abcf7d9f188e07598afd2
tnkmayumi <miiy.16f@gmail.com>
2024-11-23 17:51:36 +0900

    koredeiidesuka

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -7,15 +7,24 @@ import remarkParse from "remark-parse";
 import remarkHtml from "remark-html";
 import Image from "next/image";
 
-
-
-export default async function BlogPost({ params }: { params: { blogId: string } }) {
+export default async function BlogPost({
+  params,
+}: {
+  params: { blogId: string };
+}) {
   const { blogId } = params;
-  const filePath = path.join(process.cwd(), "src/app/blogs/posts", `${blogId}.md`);
+  const filePath = path.join(
+    process.cwd(),
+    "src/app/blogs/posts",
+    `${blogId}.md`,
+  );
   const fileContents = fs.readFileSync(filePath, "utf8");
   const { data, content } = matter(fileContents);
-  
-  const processedContent = await unified().use(remarkParse).use(remarkHtml).process(content);
+
+  const processedContent = await unified()
+    .use(remarkParse)
+    .use(remarkHtml)
+    .process(content);
   const contentHtml = processedContent.toString();
 
   return (
@@ -23,29 +32,25 @@ export default async function BlogPost({ params }: { params: { blogId: string } 
       <div className="mx-auto max-w-3xl text-base leadling-7 text-gray-700">
         {data.thumbnail && (
           <Image
-          src={data.thumbnail}
-          alt={data.title}
-          width={800}
-          height={400}
-          style={{marginBottom: "20px", objectFit: "cover" }}
+            src={data.thumbnail}
+            alt={data.title}
+            width={800}
+            height={400}
+            style={{ marginBottom: "20px", objectFit: "cover" }}
           />
         )}
         <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           {data.title}
         </h1>
-        <p className="mt-2 text-1xl">
-          {data.date}
-        </p>
-        {/* <p>タグ: {data.tags.join(", ")}</p> */}
+        <p className="mt-2 text-1xl">{data.date}</p>
         <div
           className="mt-6"
           dangerouslySetInnerHTML={{ __html: contentHtml }}
-          ></div>
+        ></div>
       </div>
     </div>
   );
 }
-
 
 // 動的ルート用のパスとデータを生成　だが以下はなくても動く、、、
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,85 +14,79 @@ export default async function Home() {
     fileNames.map(async (fileName) => {
       const filePath = path.join(postsDirectory, fileName);
       const fileContents = fs.readFileSync(filePath, "utf8");
-      const { data } = matter(fileContents); // メタデータを取得
+      const { data, content } = matter(fileContents); // メタデータを取得
 
       return {
         slug: fileName.replace(/\.md$/, ""),
         frontmatter: data,
         thumbnail: data.thumbnail,
-        // tags: data.tags || [],
+        description: data.description,
+        beginning: content.slice(0, 100),
       };
-    })
-  )
-  .then((posts) =>
-    posts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+    }),
+  ).then((posts) =>
+    // 最新日付順に並び替え
+    posts.sort(
+      (a, b) =>
+        new Date(b.frontmatter.date).getTime() -
+        new Date(a.frontmatter.date).getTime(),
+    ),
   );
 
-  // const [searchQuery, setSearchQuery] = useState("");
-  // const filteredPosts = posts.filter(
-  //   (post) =>
-  //     post.frontmatter.title.includes(searchQuery) ||
-  //     post.tags.some((tag) => tag.includes(searchQuery))
-  // );
-
-
-
-  
-  
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      {/* <input
-          type="text"
-          placeholder="検索 (タイトル/タグ)"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="mb-6 p-2 border rounded"
-        /> */}
         <div className="mx-auto max-w-2xl">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            ブログ記事一覧</h2>
+            ブログ記事一覧
+          </h2>
           <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
             {posts.map((post) => (
               <article
                 key={post.slug}
-                className="flex max-w-xl flex-col items-start justify-between"
+                className="flex w-full mx-auto flex-col items-start justify-between"
               >
-                <div className="group relative">
-                  <div className="items-center gap-x-4 text-xs">
-                  <Link 
-                      href={`/blogs/${post.slug}`}>
-                      <Image
-                      src={post.thumbnail}
-                      alt={post.frontmatter.title}
-                      width={300}
-                      height={200}
-                      style={{objectFit: "cover" }}
-                      />
-                  </Link>
-                    <div className="text-gray-500">
-                      {post.frontmatter.date}
+                <div className="flex">
+                  <div className="group relative">
+                    <div className="items-center gap-x-4 text-xs">
+                      <Link href={`/blogs/${post.slug}`}>
+                        <Image
+                          src={post.thumbnail}
+                          alt={post.frontmatter.title}
+                          width={300}
+                          height={200}
+                          style={{ objectFit: "cover" }}
+                        />
+                      </Link>
+                      <div className="text-gray-500">
+                        {post.frontmatter.date}
+                      </div>
                     </div>
+
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <Link
+                        href={`/blogs/${post.slug}`}
+                        className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
+                      >
+                        {post.frontmatter.title}
+                      </Link>
+                    </h3>
                   </div>
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <Link 
-                      href={`/blogs/${post.slug}`}
-                      className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                  </h3>
-                  <p
-                    className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"
-                    dangerouslySetInnerHTML={{ __html:`${post.frontmatter.description}`}}
-                  ></p>
+                  <div className="teamb mx-auto ml-3">
+                    <p
+                      className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"
+                      dangerouslySetInnerHTML={{
+                        __html: `${post.frontmatter.description}`,
+                      }}
+                    ></p>
+                    <div className="w-[40ch] break-words">{post.beginning}</div>
+                  </div>
                 </div>
               </article>
             ))}
           </div>
         </div>
       </div>
-      
     </div>
   );
 }
