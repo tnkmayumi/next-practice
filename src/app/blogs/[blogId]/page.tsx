e75689298d8172e4d4738c138c03c0d3a8d920d7
tnkmayumi <miiy.16f@gmail.com>
2024-11-26 19:43:47 +0900

    errorretouching

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -7,12 +7,16 @@ import remarkParse from "remark-parse";
 import remarkHtml from "remark-html";
 import Image from "next/image";
 
-export default async function BlogPost({
-  params,
-}: {
-  params: { blogId: string };
-}) {
+type BlogPostParams = {
+  params: {
+    blogId: string;
+  };
+};
+
+export default async function BlogPost({ params }: BlogPostParams) {
   const { blogId } = params;
+
+  // const { blogId } = params;
   const filePath = path.join(
     process.cwd(),
     "src/app/blogs/posts",
